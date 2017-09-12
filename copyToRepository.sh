1032c6669320d946d1f211bb6fb9176bdd04a5d0
spitfjre <spitfjre@gmx.de>
2017-09-12 12:58:39 +0200

    added twmn

--- copyToRepository.sh
+++ copyToRepository.sh
@@ -12,6 +12,7 @@ cp -r $HOME/.config/mupen64plus/* $repoDir/.config/mupen64plus/
 cp -r $HOME/.config/polybar/* $repoDir/.config/polybar/
 cp -r $HOME/.config/ranger/rc.conf $repoDir/.config/ranger/
 cp -r $HOME/.config/termite/* $repoDir/.config/termite/
+cp -r $HOME/.config/twmn/* $repoDir/.config/twmn/
 
 cp -r $HOME/Pictures/wallpaper/* $repoDir/wallpaper/
 
