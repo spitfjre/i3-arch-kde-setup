615091ce4292d46763c428147712c5a46a7fe6cc
spitfjre <spitfjre@gmx.de>
2018-02-12 12:17:35 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -69,8 +69,8 @@ sudo pip install fontawesome
 cd ..
 sudo rm -R fontawesome-python
 
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+#curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+#echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syu sublime-text
 
 yaourt -Rcns --noconfirm lightdm
